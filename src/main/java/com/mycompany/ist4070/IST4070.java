5477088d7d92358f4ee30e2da61a9be531072a42
Faith Ojochenemi Zekeli <135419841+Faithzek@users.noreply.github.com>
2025-09-19 22:18:44 +0300

    initial project files

---
+++ src/main/java/com/mycompany/ist4070/IST4070.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ist4070;
+
+/**
+ *
+ * @author Faith O Zekeli
+ */
+public class IST4070 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
